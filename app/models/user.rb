2268b8635e3267a30ede3b14a2eef4891f11e322
Kira Corbett <kiracorbett00@gmail.com>
2021-05-02 16:28:57 -0700

    WIP: Buying a Subscription

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,80 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :masqueradable, :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable, :omniauthable
 
-  has_one_attached :avatar
   has_person_name
 
-  has_many :notifications, as: :recipient
+  has_many :notifications, foreign_key: :recipient_id
   has_many :services
+  has_many :subscriptions
+  has_many :charges
+
+  def subscribed?
+    subscription && subscription.active?
+  end
+
+  def subscription
+    subscriptions.last
+  end
+
+  def subscribe(plan, options={})
+    stripe_customer if !stripe_id?
+
+    args = {
+      customer: stripe_id,
+      #items: [{price: plan.stripe_price_id}],
+      items: [{ plan: plan }],
+      expand: ['latest_invoice.payment_intent'],
+      off_session: true,
+    }.merge(options)
+
+    args[:trial_from_plan] = true if !args[:trial_period_days]
+
+    sub = Stripe::Subscription.create(args)
+
+    subscription = subscriptions.create(
+      stripe_id: sub.id,
+      stripe_plan: plan,
+      status: sub.status,
+      trial_ends_at: (sub.trial_end ? Time.at(sub.trial_end) : nil),
+      ends_at: nil,
+    )
+
+    if sub.status == "incomplete" && ["requires_action", "requires_payment_method"].include?(sub.latest_invoice.payment_intent.status)
+      raise PaymentIncomplete.new(sub.latest_invoice.payment_intent), "Subscription requires authentication"
+    end
+
+    subscription
+  end
+
+  def create_setup_intent
+    stripe_customer if !stripe_id?
+    Stripe::SetupIntent.create(customer: stripe_id)
+  end
+
+  def update_card(payment_method_id)
+    stripe_customer if !stripe_id?
+
+    payment_method = Stripe::PaymentMethod.attach(payment_method_id, { customer: stripe_id })
+    Stripe::Customer.update(stripe_id, invoice_settings: { default_payment_method: payment_method.id })
+
+    update(
+      card_brand: payment_method.card.brand.titleize,
+      card_last4: payment_method.card.last4,
+      card_exp_month: payment_method.card.exp_month,
+      card_exp_year: payment_method.card.exp_year,
+    )
+  end
+
+  def stripe_customer
+    if stripe_id
+      Stripe::Customer.retrieve(stripe_id)
+    else
+      customer = Stripe::Customer.create(
+        email: email,
+        name: name,
+      )
+      update(stripe_id: customer.id)
+      customer
+    end
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Movie.create(title: "The Matrix", video_url: "secret")
+
+Plan.create(name: "Small", stripe_price_id: 'price_1Ijd6VJJVcje0qlQjxW3GpH4', stripe_id: "prod_JMLo0GIfhdFloU", amount: 2500, interval: "month")
+Plan.create(name: "Small", stripe_price_id: 'price_1Ijd6IJJVcje0qlQGhWkBLqT', stripe_id: "prod_JMLnDIFa73ykxN", amount: 25000, interval: "year")
+Plan.create(name: "Large", stripe_price_id: 'price_1Ijd60JJVcje0qlQ0OfPyIE7', stripe_id: "prod_JMKxvzDBtIkVTk", amount: 10000, interval: "month")
+Plan.create(name: "Large", stripe_price_id: 'price_1IjcHYJJVcje0qlQt3bjl6ub', stripe_id: "prod_JMLnidyBov21Of", amount: 100000, interval: "year")
